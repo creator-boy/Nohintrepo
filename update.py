20e07eeb8b283455b8ed6c7b6db7b829d6047fc9
creator-boy <89370797+creator-boy@users.noreply.github.com>
2022-06-03 14:27:41 +0530

    Update update.py

--- update.py
+++ update.py
@@ -57,6 +57,6 @@ if UPSTREAM_REPO is not None:
                      && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
 
     if update.returncode == 0:
-        log_info('Successfully updated with latest commit from UPSTREAM_REPO')
+        log_info('Successfully updated with latest features from the Upstream Repo')
     else:
         log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
